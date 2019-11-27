e31100be100ff4cc090a2c927c6ab52cf31b78bf
LeeVaughn <dlvprogramming@gmail.com>
2019-11-26 20:12:29 -0800

    create Deck class and import shuffle

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
+from random import shuffle
+
 class Card:
   suits = ["spades", "hearts", "diamonds", "clubs"]
   # first two items are None so that index matches card value
   values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
   def __init__(self, value, suit):
-    """suits and values should be integers"""
+    # suits and values should be integers
     self.value = value
     self.suit = suit
 
@@ -33,3 +35,18 @@ class Card:
   # returns the value and suit of the Card object
   def __repr__(self):
     return self.values[self.value] + " of " + self.suits[self.suit]
+
+class Deck:
+  def __init__(self):
+    self.cards = []
+    # creates a list of cards, first loop is for each card rank, second loop is for each card suit
+    for i in range(2, 15):
+      for j in range(4):
+        self.cards.append(Card(i, j))
+    shuffle(self.cards)
+
+  # removes and returns a card from the cards list
+  def rm_card(self):
+    if len(self.cards) == 0:
+      return
+    return self.cards.pop()
