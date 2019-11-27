c094b77496eb04fcbe4c8ad2a30464069671e432
LeeVaughn <dlvprogramming@gmail.com>
2019-11-26 20:40:31 -0800

    create Game class and fix error in Player class

--- game.py
+++ game.py
@@ -51,8 +51,67 @@ class Deck:
       return
     return self.cards.pop()
 
-  def Player:
-    def __init__(self, name):
-      self.wins = 0
-      self.card = None
-      self.name = name
+class Player:
+  def __init__(self, name):
+    self.wins = 0
+    self.card = None
+    self.name = name
+
+class Game:
+  def __init__(self):
+    # starts game by getting player names, creating Deck object, and creating players
+    name1 = input("Player 1 name ")
+    name2 = input("Player 2 name ")
+    self.deck = Deck()
+    self.p1 = Player(name1)
+    self.p2 = Player(name2)
+
+  # prints the winner of each round of the game
+  def wins(self, winner):
+    w = "{} wins this round"
+    w = w.format(winner)
+    print(w)
+
+  # prints the results the outcome of each round of card draws
+  def draw(self, p1n, p1c, p2n, p2c):
+    d = "{} drew {} while {} drew {}"
+    d = d.format(p1n, p1c, p2n, p2c)
+    print(d)
+
+  # starts the game
+  def play_game(self):
+    cards = self.deck.cards
+    print("Beginning War!")
+    # continue the game as long as there are at least two or more cards left in the deck
+    while len(cards) >= 2:
+      m = "Press q to quit or any " + "key to play!"
+      response = input(m)
+      if response == "q":
+        break
+      p1c = self.deck.rm_card()
+      p2c = self.deck.rm_card()
+      p1n = self.p1.name
+      p2n = self.p2.name
+      self.draw(p1n, p1c, p2n, p2c)
+      if p1c > p2c:
+        self.p1.wins += 1
+        self.wins(self.p1.name)
+      else:
+        self.p2.wins += 1
+        self.wins(self.p2.name)
+
+    win = self.winner(self.p1, self.p2)
+
+    # print result of the game
+    print("War is over. {} wins".format(win))
+
+  # returns the player who won the most rounds
+  def winner(self, p1, p2):
+    if p1.wins > p2.wins:
+      return p1.name
+    if p1.wins < p2.wins:
+      return p2.name
+    return "It was a tie!"
+
+game =Game()
+game.play_game()
